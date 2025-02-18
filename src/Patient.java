2c39a5e6265f9cce39a05db57a80ef89684a2ebd
Zack <zacharycollier36@protonmail.com>
2025-02-18 12:50:48 -0330

    Changed prescription ArrayList to use prescription ids instead of prescription objects
    - Changed prescription ArrayList and related methods to use prescription ids instead of prescription objects, preventing the case where an infinite number of patient and prescription objects are created via constructor

--- src/Patient.java
+++ src/Patient.java
@@ -3,34 +3,34 @@ import java.util.ArrayList;
 public class Patient extends Person {
     //attributes
     private ArrayList<String> medications = new ArrayList<String>();
-    private ArrayList<String> prescriptions = new ArrayList<String>();
+    private ArrayList<Integer> prescriptionIds = new ArrayList<Integer>();
 
     //methods, replace arraylist string with arraylist medications/prescriptions once those classes are done
-    public Patient(int id, String name, int age, String phone, ArrayList<String> medications, ArrayList<String> prescriptions){
+    public Patient(int id, String name, int age, String phone, ArrayList<String> medications, ArrayList<Integer> prescriptions){
         super(id, name, age, phone);
         this.medications = medications;
-        this.prescriptions = prescriptions;
+        this.prescriptionIds = prescriptions;
     }
 
     public void setMedications(ArrayList<String> meds){
         this.medications = meds;
     }
-    public void setPrescriptions(ArrayList<String> prescripts){
-        this.prescriptions = prescripts;
+    public void setPrescriptions(ArrayList<Integer> prescripts){
+        this.prescriptionIds = prescripts;
     }
 
     public ArrayList<String> getMedications(){
         return this.medications;
     }
-    public ArrayList<String> getPrescriptions(){
-        return this.prescriptions;
+    public ArrayList<Integer> getPrescriptions(){
+        return this.prescriptionIds;
     }
 
     public void addMedication(String med){
         this.medications.add(med);
     }
-    public void addPrescription(String pres){
-        this.prescriptions.add(pres);
+    public void addPrescription(int pres){
+        this.prescriptionIds.add(pres);
     }
     public void removeMedication(String med){
         int index = this.medications.indexOf(med);
@@ -40,10 +40,10 @@ public class Patient extends Person {
             System.out.println("Medication not found. Please ensure that you have the correct spelling and capitalization.");
         }
     }
-    public void removePrescription(String pres){
-        int index = this.prescriptions.indexOf(pres);
+    public void removePrescription(int pres){
+        int index = this.prescriptionIds.indexOf(pres);
         if (index >= 0){
-            this.prescriptions.removeIf(n -> n == pres);
+            this.prescriptionIds.removeIf(n -> n == pres);
         } else {
             System.out.println("Prescription not found. Please ensure that you have the correct spelling and capitalization.");
         }
@@ -56,12 +56,13 @@ public class Patient extends Person {
             return "Medication not found. Please ensure that you have the correct spelling and capitalization.";
         }
     }
-    public String getOnePrescription(String pres){
-        int index = this.prescriptions.indexOf(pres);
+    public int getOnePrescription(int pres){
+        int index = this.prescriptionIds.indexOf(pres);
         if (index >= 0){
-            return this.prescriptions.get(index);
+            return this.prescriptionIds.get(index);
         } else {
-            return "Prescription not found. Please ensure that you have the correct spelling and capitalization.";
+            System.out.println("Prescription not found. Please ensure that you have the correct spelling and capitalization.");
+            return -1;
         }
     }
 
